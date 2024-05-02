4edaca4a7050842816db78ede594b43303ef2763
alexandree3 <aleksandr.suharev2805@mail.ru>
2024-05-02 20:12:47 +0300

    Lab_1

---
+++ OOP LAB1/OOP LAB1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_LAB1
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
---
+++ OOP LAB1/TASK3/Class3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK3
+{
+    internal class Class3
+    {
+        static void Main()
+        {
+            TreeNode root = new TreeNode(1);
+            TreeNode child1 = new TreeNode(2);
+            TreeNode child2 = new TreeNode(3);
+            TreeNode grandchild1 = new TreeNode(4);
+            TreeNode grandchild2 = new TreeNode(5);
+
+            root.AddChild(child1);
+            root.AddChild(child2);
+            child1.AddChild(grandchild1);
+            child1.AddChild(grandchild2);
+
+            root.PrintChildrenValues();
+        }
+    }
+}
