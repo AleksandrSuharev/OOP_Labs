b93f87f865edca35a493e5c5894ec7a823d8a55f
AleksandrSuharev <aleksandr.suharev2805@mail.ru>
2024-05-02 21:21:46 +0300

    Lab_2

---
+++ OOP_LAB2/OOP_LAB2_Sync'/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Request
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var urls = new[] { "https://api.hh.ru/openapi/redoc#section/Obshaya-informaciya/Vybor-sajta", "https://discord.com/idasdad", "https://edu.stankin.ru/" };
+
+            var startTime = DateTime.Now;
+            for (int i = 0; i < urls.Length; i++)
+            {
+                var response = client.GetAsync(urls[i]).Result;/// ВОПРОС 
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"URL{i + 1}: Error: {response.StatusCode}");
+                }
+                else
+                {
+                    var JSON = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine($"URL{i + 1}: {JSON}");
+                }
+            }
+            var endTime = DateTime.Now;
+            Console.WriteLine($"Total time: {endTime - startTime}");
+        }
+    }
+}
